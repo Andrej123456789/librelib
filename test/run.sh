e5773326f276768b5142e2816457dfa3ede41bff
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2024-08-17 14:49:23 +0200

    Removed unused file

--- test/run.sh
+++
@@ -1,9 +0,0 @@
-test() {
-    gcc -Wall -Wextra -Werror -pedantic -g -llibrelib test.c -o test
-}
-
-test2() {
-    gcc -fno-stack-protector -ffreestanding -nostdlib -g -llibrelib test2.c -o test2
-}
-
-"$@"
